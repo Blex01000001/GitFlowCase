c15689ac603e6a886884a9312998282c1c59cab6
MSI\USER <Alex@gmail.com>
2024-05-25 21:58:10 +0800

    register codeing2

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace GitFlowCase
             //add backend/servise folder
             //modify/create code in Program
             //fix bug in Program
-            //register coding
+            //register coding2
         }
     }
 }
